f4b981c1eb264f69499113234ff52b8fb3cb34ac
Sam King <belven000@gmail.com>
2019-02-07 22:23:21 +0000

    Fixed issues with combat groups

--- jobs/militia/militia.lua
+++ jobs/militia/militia.lua
@@ -1,8 +1,6 @@
 local MilitiaClass = class()
 
 local CombatJob = radiant.mods.require 'stonehearth.jobs.combat_job'
-local BaseJob = require 'stonehearth.jobs.base_job'
 radiant.mixin(MilitiaClass, CombatJob)
-radiant.mixin(MilitiaClass, BaseJob)
 
 return MilitiaClass
